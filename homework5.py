0f343cd201e476cabeb15f4e8db10e21d546c373
creativealex1 <rudnik.2002@inbox.ru>
2024-06-12 17:17:42 +0300

    Add files via upload

---
+++ homework5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, True , 'String'
+print (immutable_var)
+#immutable_var [0] = 1000  Объяснение к 3 заданию - tuple не поддерживает обращение по элементам
+#print (type(immutable_var))
+mutable_list = [2, False, "Tuple"]
+print (mutable_list)
+mutable_list [2] = 'List'
+print (mutable_list)
